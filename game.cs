489d2b89bd42e5865b4f2794214c0dcab78bb9c6
Gabriel <almendrasgabriel11@gmail.com>
2023-10-20 19:01:50 -0400

    Codigo crudo actualizado

--- game.cs
+++ game.cs
@@ -39,7 +39,7 @@ namespace ProGrafica
 
             cuarto.translate(1.0, 2.0, 0.0);
             cuarto.scale(1.3f);
-            cuarto.rotate("x", 45.0f);
+            //cuarto.rotate("z", 45.0f);
             GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
             GL.Enable(EnableCap.DepthTest);
